d00b4270ec1f357675f547eef0a4867cbc1bf9c5
BG <BG@BG-PC.station>
2015-04-14 23:48:04 +0200

    initial checkin of sales tool

---
+++ LocalSalesToolRepository/SalesTool/src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SalesData data = new SalesData();
+		displayGreeting();
+		data.display();
+
+	}
+
+	private static void displayGreeting() {
+		// TODO Auto-generated method stub
+		System.out.println("HELLO HAPPY SALES PEOPLE!");
+		System.out.println("THIS APP SHOWS SALES DATA");
+		
+	}
+
+}
---
+++ LocalSalesToolRepository/SalesTool/src/SalesData.java
@@ -0,0 +1,12 @@
+
+public class SalesData {
+	
+	int[] data = {0, 4, 42};
+
+	public void display() {
+		// TODO Auto-generated method stub
+		System.out.println("TODO: implement data display");
+		
+	}
+
+}
