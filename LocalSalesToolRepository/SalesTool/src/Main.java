a35ed587a8707c4fa06aeb64c829a30ec89f9519
Mario Toscano <v-matoscano@expedia.com>
2015-04-15 15:55:37 +0200

    Fix case on initial greeting display.

--- LocalSalesToolRepository/SalesTool/src/Main.java
+++ LocalSalesToolRepository/SalesTool/src/Main.java
@@ -11,8 +11,9 @@ public class Main {
 
 	private static void displayGreeting() {
 		// TODO Auto-generated method stub
-		System.out.println("HELLO HAPPY SALES PEOPLE!");
-		System.out.println("THIS APP SHOWS SALES DATA");
+		System.out.println("Hello Happy Sales People!");
+		System.out.println("This app shows sales data.");
+		System.out.println("--------------------------");
 		System.out.println("Test 1");
 		System.out.println("Test 2");
 		
